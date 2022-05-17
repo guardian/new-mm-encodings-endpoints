dcbba90d2e6e8ac71e9f91023a852dd6e6d3ab5b
andy_gallagher <andy.gallagher@theguardian.com>
2022-05-17 21:59:07 +0100

    Test fix

--- common/poster_image_test.go
+++ common/poster_image_test.go
@@ -8,8 +8,12 @@ import (
 Tests that the correct URL is returned for the poster image
 */
 func TestGeneratePosterImageURL(t *testing.T) {
-	result, _ := GeneratePosterImageURL("https://cdn.theguardian.tv/HLS/2018/06/06/091101BangladeshVillages.m3u8")
+	result, _ := GeneratePosterImageURL("https://cdn.theguardian.tv/HLS/2018/06/06/091101BangladeshVillages.m3u8", false)
 	if result != "https://cdn.theguardian.tv/HLS/2018/06/06/091101BangladeshVillages_poster.jpg" {
+		t.Errorf("Unexpected output: %s", result)
+	}
+	result, _ = GeneratePosterImageURL("https://cdn.theguardian.tv/HLS/2018/06/06/091101BangladeshVillages.m3u8", true)
+	if result != "https://cdn.theguardian.tv/HLS/2018/06/06/091101BangladeshVillages_poster.png" {
 		t.Errorf("Unexpected output: %s", result)
 	}
 }
@@ -18,7 +22,7 @@ func TestGeneratePosterImageURL(t *testing.T) {
 Tests an error is returned if the URL can not be parsed
 */
 func TestGeneratePosterImageURLError(t *testing.T) {
-	_, err := GeneratePosterImageURL("test")
+	_, err := GeneratePosterImageURL("test", false)
 	if err == nil {
 		t.Error("GeneratePosterImageURL returned no error for an invalid URL")
 	}
